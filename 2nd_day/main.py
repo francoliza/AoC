167f3f7a549bafec195d28786ff86bf5da228c29
Franco Liza <franco@debian-BULLSEYE-live-builder-AMD64>
2021-12-07 23:45:54 -0300

    part 2 of day 2 solved
    ;

--- 2nd_day/main.py
+++ 2nd_day/main.py
@@ -23,12 +23,56 @@ class Coordinates:
     def current_y(self):
         return self.y
 
+class Coordinates_with_aim():
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.aim = 0
+
+    def forward(self, distance):
+        distance = int(distance)
+        self.x += distance
+        self.y += self.aim * distance
+
+    def up(self, distance):
+        distance = int(distance)
+        self.aim -= distance
+
+    def down(self, distance):
+        distance = int(distance)
+        self.aim += distance
+
+    def current_x(self):
+        return self.x
+
+    def current_y(self):
+        return self.y
 
 def day_2_part_1():
     file1 = open('input_1.txt', 'r')
     lines = file1.readlines()
 
     coord = Coordinates()
+
+    for line in lines:
+        move, value = line.split()
+
+        if move == 'forward':
+            coord.forward(value)
+
+        if move == 'up':
+            coord.up(value)
+
+        if move == 'down':
+            coord.down(value)
+
+    return coord.current_x() * coord.current_y()
+
+def day_2_part_2():
+    file1 = open("input_2.txt", "r")
+    lines = file1.readlines()
+
+    coord = Coordinates_with_aim()
 
     for line in lines:
         move, value = line.split()
@@ -52,6 +96,9 @@ def main():
     #print("up(2): X = {0:d}, Y = {1:d}".format(coord.current_x(), coord.current_y()))
 
     result = day_2_part_1()
+    print("result: ", result)
+
+    result = day_2_part_2()
     print("result: ", result)
 
 if __name__ == '__main__':
